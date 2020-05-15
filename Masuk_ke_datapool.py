a9aa09088fe40b1a015f45d3827525469809aeba
aldrinmarbun <45108331+aldrinmarbun@users.noreply.github.com>
2020-05-15 08:28:10 +0700

    Add files via upload

---
+++ Masuk_ke_datapool.py
@@ -0,0 +1,80 @@
+import hashlib
+import datetime
+import pymysql
+
+#Connect Dulu
+conn = pymysql.connect(host="localhost", user="root", passwd="", db="db_python")
+data1 = ['budi','tono','dwi']
+data2 = ['dion','febrianto','jumi']
+data = data2
+cur = conn.cursor()
+query = "SELECT * FROM data_ins"
+cur.execute(query)
+check = cur.fetchone()
+i = 0
+#Check kalau ada data di table sementara atau tidak
+if check is None :
+    print('Inserting Data')
+    while i <= (len(data)-1) :
+        id = i + 1
+        val = str(data[i])
+        time = str(datetime.datetime.now())
+        print(id,val,time)
+        query = "INSERT INTO data_ins(id,data,timestamp) VALUES(%s,%s,%s)"
+        args = (str(id),str(val),str(time))
+        cur.execute(query, args)
+        conn.commit()
+        i += 1
+else :
+    print('Table not empty')
+
+text = input("Data has been inserted to temporary. Press enter!")
+# Ambil semua data yang ada di table sementara
+query = "SELECT id,data,timestamp FROM data_ins ORDER BY timestamp"
+cur.execute(query)
+insert = cur.fetchall()
+
+# Pemeriksaan data pool
+query = "SELECT count(*) as tot FROM data_pool"
+cur.execute(query)
+mr = cur.fetchone()[0]
+if mr != 0 :
+    query = "Select id from data_pool order by id desc"
+    cur.execute(query)
+    resId = cur.fetchone()[0]
+    newid = int(resId)
+else :
+    newid = 0
+
+for i in range(len(insert)) :
+    newid = newid + 1
+    query = "SELECT count(*) as tot FROM data_pool"
+    cur.execute(query)
+    data = cur.fetchall()[0][0]
+    prevhash = str(0)
+    if data != 0:
+        query = "Select hash from data_pool order by id desc"
+        cur.execute(query)
+        prevhash = str(cur.fetchone()[0])
+    else:
+        prevhash = str(0)
+    hash = str(hashlib.sha224((str(newid)+str(insert[i][1])+str(insert[i][2])+prevhash).encode('utf-8')).hexdigest())
+
+    ins_dp = {'id': str(newid),
+            'data': str(insert[i][1]),
+            'timestamp': str(insert[i][2]),
+            'prevhash': str(prevhash),
+            'hash': str(hash)}
+
+    query = "Insert INTO data_pool (id, data, timestamp, prevhash, hash) values (%s, %s, %s, %s, %s)"
+    value = (ins_dp['id'], ins_dp['data'], ins_dp['timestamp'], ins_dp['prevhash'],
+           str(ins_dp['hash']))
+    cur.execute(query, value)
+    query = "DELETE FROM data_ins WHERE id = %s" % insert[i][0]
+    cur.execute(query)
+    conn.commit()
+
+print('Data has been inserted to Data Pool')
+cur.close()
+conn.close()
+
